83852cc82a166ddf62a34af356223ec12da774e8
Douglas Castro <douglascastrorj@gmail.com>
2018-09-22 12:56:23 -0300

    finalizando componente de scroll horizontal para filtro de categorias

--- src/components/views/Home/index.js
+++ src/components/views/Home/index.js
@@ -1,28 +1,50 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { navigatorDrawer, navigatorDeepLink } from '../../utils/misc';
+import HorizontalScroll from './horizontal_scroll_icons';
 
 class Home extends Component {
 
   constructor(props){
     super(props);
+
+    this.state = {
+      categories:['All', 'Sports', 'Music', 'Clothing', 'Eletronics'],
+      categorySelected: "All"
+    }
+
     this.props.navigator.setOnNavigatorEvent((event) => {
       navigatorDeepLink(event, this);
       navigatorDrawer(event, this);
     })
   }
 
+  updateCategoryHandler = (categorySelected) => {
+    this.setState({
+      categorySelected
+    })
+  }
+
   render() {
     return (
-      <View>
-        <Text>Home!</Text>
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <HorizontalScroll
+            categories={this.state.categories}
+            categorySelected={this.state.categorySelected}
+            updateCategoryHandler={this.updateCategoryHandler}
+          />
+        </View>
+      </ScrollView>
+      
     );
   }
 }
 
 const styles = StyleSheet.create({
- 
+  container:{
+    marginTop: 5,
+  }
 });
 
 
